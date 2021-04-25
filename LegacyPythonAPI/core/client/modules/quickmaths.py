15c8f2d5ce000650cbf2e7c1d966328cf2e69892
Nixon <43715558+nixon-nyx@users.noreply.github.com>
2021-04-25 10:45:58 +0800

    adding essential VFX graph for audio visualizer (will be ported into its own package in the future)

--- LegacyPythonAPI/core/client/modules/quickmaths.py
+++ LegacyPythonAPI/core/client/modules/quickmaths.py
@@ -1,61 +1,52 @@
+from __future__ import annotations
 import re
 import random
 import time
 import threading
 
+levels:list[dict] = [
+  {
+    "symbols": ["+", "-"],
+    "range1": 1,
+    "range2": 10,
+  },
+  {
+    "range1": 10,
+    "range2": 20,
+    "symbols": ["+", "-"]
+  },
+  {
+    "range1": 10,
+    "range2": 20,
+    "symbols": ["+", "-", "*"]
+  },
+  {
+    "range1": 30,
+    "range2": 50,
+    "symbols": ["+", "-", "*"]
+  },
+  {
+    "range1": 50,
+    "range2": 100,
+    "symbols": ["+", "-", "*"]
+  }
+]
 
-def level1():
-  num1 = random.randint(1, 10)
-  num2 = random.randint(1, 10)
-  symbols = ["+", "-"]
+def level(range1:int, range2:int, symbols:list[str]=["+", "-"]) -> tuple[int, int, str]:
+  num1 = random.randint(range1, range2)
+  num2 = random.randint(range1, range2)
   cal_symbol = random.choice(symbols)
   return num1, num2, cal_symbol
 
-def level2():
-  num1 = random.randint(10, 20)
-  num2 = random.randint(10, 20)
-  symbols = ["+", "-"]
-  cal_symbol = random.choice(symbols)
-  return num1, num2, cal_symbol
-
-def level3():
-  num1 = random.randint(10, 20)
-  num2 = random.randint(10, 20)
-  symbols = ["+", "-", "*"]
-  cal_symbol = random.choice(symbols)
-  return num1, num2, cal_symbol
-
-def level4():
-  num1= random.randint(30, 50)
-  num2 = random.randint(30, 50)
-  symbols = ["+", "-", "*"]
-  cal_symbol = random.choice(symbols)
-  return num1, num2, cal_symbol
-
-def level5():
-  num1 = random.randint(50, 100)
-  num2 = random.randint(50, 100)
-  symbols = ["+", "-", "*"]
-  cal_symbol = random.choice(symbols)
-  return num1, num2, cal_symbol
-
-def calculation(num1, num2, cal_symbol):
-  if cal_symbol == "+":
-    ans = num1 + num2
-  elif cal_symbol == "-":
-    ans = num1 - num2
-  elif cal_symbol == "*":
-    ans = num1 * num2
-  elif cal_symbol == "/":
-    ans = num1 / num2
-  return ans
+def calculation(num1:int, num2:int, cal_symbol:str) -> int:
+  return eval(f"{num1} {cal_symbol} {num2}")
 
 def handle(text, Mic, Agent):
   endgame = False
   rounds = 0
   start_time = time.time()
   total_time = 120 
-  Mic.say("Welcome to QuickMaths, say start if you want to start a new game")
+  Mic.say("Welcome to QuickMaths, say start if you want to start level_details new game")
   
   user_input = Mic.active_listen()
 
@@ -68,60 +59,56 @@ def handle(text, Mic, Agent):
 
   while endgame == False:
     rounds += 1
+    level_details:tuple[int, int, str]
     if rounds < 6:
-      a = level1()
+      level_details = level(**levels[0])
     elif rounds < 11:
-      a = level2()
+      level_details = level(**levels[1])
     elif rounds < 20:
-      a = level3()
+      level_details = level(**levels[2])
     elif rounds < 40:
-      a = level4()
-    elif rounds < 50:
-      a = level5()
+      level_details = level(**levels[3])
+    else:
+      level_details = level(**levels[4])
 
-    num1, num2, cal_symbol = a
+    num1, num2, cal_symbol = level_details # type: ignore
     ans = calculation(num1, num2, cal_symbol)
 
     time_past = time.time() - start_time
     time_left = total_time - time_past
 
-    print(f"answer for {num1} {cal_symbol} {num2}? \t{round(time_left)}\n")
+    Agent._print(f"answer for {num1} {cal_symbol} {num2}? \t{round(time_left)}\n")
     while True:
       if time_left > 0:
         while True:
-          userresponse = Mic.active_listen()
-          if userresponse == int:
-            userans = userresponse
-            print (userresponse)
+          user_response = Mic.active_listen()
+
+          if user_response.isdigit():
+            user_ans = int(user_response)
+            Agent._print(user_response)
+
+            if user_ans == ans:
+              total_time += 5
+              Agent._print("good")
+              rounds += 1
+            else:
+              total_time -= 5
+              Agent._print(f"bad {ans}")
+
             break
-          elif userresponse != int:
-            try:
-              userans = int(userresponse)
-              print (userresponse)
-              break
-            except:
-              print (userresponse)
-              continue
-        if userans == ans:
-          total_time += 5
-          print("good")
-          rounds += 1
-          break
-        else:
-          total_time -= 5
-          print(f"bad {ans}")
-          break
+
+          else:
+            user_ans = user_response
+            total_time -= 5
+
       else:
-        print(f"Game Over Your Score is {rounds}")
+        Agent._print(f"Game Over Your Score is {rounds}")
         endgame = True
         break
 
 def isValid(text):
   # check if text is valid
-  return (bool(re.search(r'\bquick | maths\b', text, re.IGNORECASE)))
-
-
-# if user_input == "start"
+  return bool(re.search(r'\bquick | maths\b', text, re.IGNORECASE))
 
 
 
